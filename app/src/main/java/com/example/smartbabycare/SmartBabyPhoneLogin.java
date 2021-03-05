a2200df997879651d1a8efe87de89b5c3547dd43
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-06 00:29:08 +0300

    set adapter for spiner

--- app/src/main/java/com/example/smartbabycare/SmartBabyPhoneLogin.java
+++ app/src/main/java/com/example/smartbabycare/SmartBabyPhoneLogin.java
@@ -3,12 +3,34 @@ package com.example.smartbabycare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+import com.example.smartbabycare.model.CountryData;
 
 public class SmartBabyPhoneLogin extends AppCompatActivity {
+
+    private Spinner spinnerCountries;
+    private EditText etPhoneNumber;
+
+    private Button btn_login;
+
+    private ProgressBar progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smart_baby_phone_login);
+
+        spinnerCountries = findViewById(R.id.spinnerCountries);
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        btn_login = findViewById(R.id.btn_login);
+        progressbar = findViewById(R.id.progressbar);
+
+        spinnerCountries.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, CountryData.countryNames));
+
     }
 }
