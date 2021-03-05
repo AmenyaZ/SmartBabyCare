50ee3f65018965c3038a0b35d0642c4c97aedac0
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-06 00:46:59 +0300

    Launcher Activity Changed to Login Activity

--- app/src/main/java/com/example/smartbabycare/SmartBabyPhoneLogin.java
+++ app/src/main/java/com/example/smartbabycare/SmartBabyPhoneLogin.java
@@ -37,7 +37,7 @@ public class SmartBabyPhoneLogin extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(this, VerifyActivity.class));
+                startActivity(new Intent(SmartBabyPhoneLogin.this, VerifyActivity.class));
                 finish();
             }
         });
