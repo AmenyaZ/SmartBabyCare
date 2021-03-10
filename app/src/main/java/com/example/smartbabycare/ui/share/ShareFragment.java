4a7a74beb20ebd96a1c22bdf1db788e27a33664a
AmenyaZ <zamenya98@gmail.com>
2021-03-10 12:24:12 +0300

    packaging

--- app/src/main/java/com/example/smartbabycare/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/smartbabycare/ui/share/ShareFragment.java
@@ -1,4 +1,4 @@
-package com.example.smartbabycare;
+package com.example.smartbabycare.ui.share;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.smartbabycare.R;
 
 /**
  * A simple {@link Fragment} subclass.
