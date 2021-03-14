e817825d923e71c7120fdc401c605fbd13d68ac5
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-15 00:21:09 +0300

    Cheking authenticity of code

--- app/src/main/java/com/example/smartbabycare/VerifyActivity.java
+++ app/src/main/java/com/example/smartbabycare/VerifyActivity.java
@@ -1,13 +1,17 @@
 package com.example.smartbabycare;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -38,6 +42,15 @@ public class VerifyActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String code = etCode.getText().toString().trim();
+
+                if (code.isEmpty() || code.length() < 6) {
+
+                    etCode.setError("Enter code...");
+                    etCode.requestFocus();
+                    return;
+                }
+                progressbar.setVisibility(View.VISIBLE);
+                verifyCode(code);
 
             }
         });
