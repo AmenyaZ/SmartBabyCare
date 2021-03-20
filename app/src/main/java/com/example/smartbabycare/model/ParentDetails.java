670539da02dedb4596954082d0a81f5d970a709b
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-21 01:37:43 +0300

    Implemented the model Class

--- app/src/main/java/com/example/smartbabycare/model/ParentDetails.java
+++ app/src/main/java/com/example/smartbabycare/model/ParentDetails.java
@@ -1,4 +1,47 @@
 package com.example.smartbabycare.model;
 
 public class ParentDetails {
+    public  String mName, mEmail, DoB,mPhonenumber;
+
+    public ParentDetails() {
+    }
+
+    public ParentDetails(String mName, String mEmail, String doB, String mPhonenumber) {
+        this.mName = mName;
+        this.mEmail = mEmail;
+        DoB = doB;
+        this.mPhonenumber = mPhonenumber;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmEmail() {
+        return mEmail;
+    }
+
+    public void setmEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public String getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(String doB) {
+        DoB = doB;
+    }
+
+    public String getmPhonenumber() {
+        return mPhonenumber;
+    }
+
+    public void setmPhonenumber(String mPhonenumber) {
+        this.mPhonenumber = mPhonenumber;
+    }
 }
