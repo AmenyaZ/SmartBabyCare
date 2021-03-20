00acaa8a82726a56c182c2528d148e1bb900a81d
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-21 00:44:43 +0300

    Added new ParentsDetailsActivity

---
+++ app/src/main/java/com/example/smartbabycare/ParentsDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartbabycare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParentsDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parents_details)
+    }
+}
