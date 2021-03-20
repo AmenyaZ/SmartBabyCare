b292bc7a60694633daee32802c9094f9777fd408
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-21 02:17:19 +0300

    varification  >> update profile

--- app/src/main/java/com/example/smartbabycare/VerifyActivity.java
+++ app/src/main/java/com/example/smartbabycare/VerifyActivity.java
@@ -106,7 +106,7 @@ public class VerifyActivity extends AppCompatActivity {
 
                     builder.setView(view);
                     builder.show();
-                    Intent intent = new Intent(getApplicationContext(),ParentsDetailsActivity.class);
+                    Intent intent = new Intent(getApplicationContext(),ParentDetailsActivity.class);
                     startActivity(intent);
 
                 }
