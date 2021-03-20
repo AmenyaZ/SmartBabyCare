5eb593fbd1af3c98dac623ca9d3e36bf3f1874f6
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-21 02:35:13 +0300

    Added few changes to database Refference

--- app/src/main/java/com/example/smartbabycare/ParentDetailsActivity.java
+++ app/src/main/java/com/example/smartbabycare/ParentDetailsActivity.java
@@ -100,7 +100,7 @@ public class ParentDetailsActivity extends AppCompatActivity {
                 );
 
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                assert user != null;
+               // assert user != null;
                 String userId = user.getUid();
 
                 DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("registration").child(userId);
