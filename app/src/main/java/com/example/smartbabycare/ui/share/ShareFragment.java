0d2211a223f8ad533a05d3c752ad2f549f902911
AmenyaZ <zamenya98@gmail.com>
2021-03-24 08:48:21 +0300

    nav share implementation

--- app/src/main/java/com/example/smartbabycare/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/smartbabycare/ui/share/ShareFragment.java
@@ -1,38 +1,64 @@
 package com.example.smartbabycare.ui.share;
-
-import androidx.lifecycle.ViewModelProvider;
-
+//
+//import androidx.lifecycle.ViewModelProvider;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import com.example.smartbabycare.R;
+//
+//public class ShareFragment extends Fragment {
+//
+//    private ShareViewModel mViewModel;
+//
+//    public static ShareFragment newInstance() {
+//        return new ShareFragment();
+//    }
+//
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+//                             @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_nav_share, container, false);
+//    }
+//
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//        mViewModel = new ViewModelProvider(this).get(ShareViewModel.class);
+//        // TODO: Use the ViewModel
+//    }
+//
+//}
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smartbabycare.R;
+import com.example.smartbabycare.ui.home.HomeFragment;
 
-public class ShareFragment extends Fragment {
 
-    private ShareViewModel mViewModel;
+import java.util.Objects;
 
-    public static ShareFragment newInstance() {
-        return new ShareFragment();
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nav_share, container, false);
-    }
+public class ShareFragment extends AppCompatActivity {
+    private ImageView btn_backHome;
+    private ProgressBar progressBar;
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ShareViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_nav_about);
 
+    }
 }
