927bde5f63658c8928a0902bf48dae94177e1df5
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-28 21:17:21 +0300

    Resolving About Fragment::Fragment Cant be casted to Activity

--- app/src/main/java/com/example/smartbabycare/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/smartbabycare/ui/about/AboutFragment.java
@@ -1,42 +1,43 @@
 package com.example.smartbabycare.ui.about;
-//
-//import androidx.lifecycle.ViewModelProvider;
-//
-//import android.os.Bundle;
-//
-//import androidx.annotation.NonNull;
-//import androidx.annotation.Nullable;
-//import androidx.fragment.app.Fragment;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-//import com.example.smartbabycare.R;
-//
-//public class AboutFragment extends Fragment {
-//
-//    private AboutViewModel mViewModel;
-//
-//    public static AboutFragment newInstance() {
-//        return new AboutFragment();
-//    }
-//
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-//                             @Nullable Bundle savedInstanceState) {
-//        return inflater.inflate(R.layout.fragment_nav_about, container, false);
-//    }
-//
-//    @Override
-//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//        mViewModel = new ViewModelProvider(this).get(AboutViewModel.class);
-//        // TODO: Use the ViewModel
-//    }
-//
-//}
 
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.smartbabycare.R;
+
+public class AboutFragment extends Fragment {
+
+    private AboutViewModel mViewModel;
+
+    public static AboutFragment newInstance() {
+        return new AboutFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_nav_about, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(AboutViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
+
+/*
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -83,4 +84,4 @@ public class AboutFragment extends AppCompatActivity {
             }
         });
     }
-}
+}*/
