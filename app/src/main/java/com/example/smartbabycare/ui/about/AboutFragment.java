4c97198e46ce192f3f322fef4c41a2e30029fc40
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-29 00:32:28 +0300

    Implemented the About Fragment

--- app/src/main/java/com/example/smartbabycare/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/smartbabycare/ui/about/AboutFragment.java
@@ -2,6 +2,7 @@ package com.example.smartbabycare.ui.about;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +12,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.smartbabycare.R;
+import com.example.smartbabycare.SmartBaby;
 
 public class AboutFragment extends Fragment {
+
+    private ImageView btn_backHome;
+    private ProgressBar progressBar;
 
     private AboutViewModel mViewModel;
 
@@ -26,13 +33,32 @@ public class AboutFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_nav_about, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_backHome= view.findViewById(R.id.iv_back);
+        progressBar= view.findViewById(R.id.simpleProgressBar);
+
+        btn_backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                Intent intent = new Intent(getContext(), SmartBaby.class);
+                startActivity(intent);
+                return;
+            }
+        });
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(AboutViewModel.class);
-        // TODO: Use the ViewModel
+        // TODO: Use the ViewModel\
+
     }
 
 }
